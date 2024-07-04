9b3f11d7089415b4bd86f6570d5236bb010efdff
Tiago Souza <tiago0214@gmail.com>
2024-07-04 23:17:34 +0100

    att

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import styles from './Sidebar.module.css'
 import { PencilLine } from '@phosphor-icons/react'
 
@@ -10,7 +11,7 @@ export function Sidebar(){
       />
       
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/tiago0214.png" />
+        <Avatar src="https://github.com/tiago0214.png"/>
 
         <strong>Tiago Souza</strong>
         <span>Web developer</span>
