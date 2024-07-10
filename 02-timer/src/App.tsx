2868206d9aa668cf9e245ff69f5545bf8d027169
Tiago Souza <tiago0214@gmail.com>
2024-07-10 17:15:00 +0100

    att

---
+++ 02-timer/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/Button"
+
+export function App() {
+  return (
+    <>
+    <Button color= 'primary'/>
+    <Button color= 'secondary'/>
+    <Button color= 'danger'/>
+    <Button color= 'success'/>
+    </>
+  )
+}
