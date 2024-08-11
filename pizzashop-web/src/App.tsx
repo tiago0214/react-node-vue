f2e4c3ab1b534b279ee23806c9043834e31701a9
Tiago Souza <tiago0214@gmail.com>
2024-08-11 22:58:47 +0100

    att

--- pizzashop-web/src/App.tsx
+++ pizzashop-web/src/App.tsx
@@ -1,9 +1,17 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
 import { routes } from './routes'
 
 export function App() {
-  return <RouterProvider router={routes} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={routes} />
+    </HelmetProvider>
+  )
 }
+// %s => means: where every page title will be replaced it
+// | pizza.shop => will be display in all pages
--- pizzashop-web/src/pages/auth/sign-in.tsx
+++ pizzashop-web/src/pages/auth/sign-in.tsx
@@ -1,3 +1,21 @@
+import { Helmet } from 'react-helmet-async'
+
 export function SignIn() {
-  return <h1>SignIn</h1>
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="flex w-[350px] flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tighter">
+              Acessar Painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
