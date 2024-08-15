2c63492495b74dac12240bcd09f1eae017a0d23b
Tiago Souza <tiago0214@gmail.com>
2024-08-15 15:10:53 +0100

    update

---
+++ nodeJS/01-fundamentos-nodejs/src/stream/http-server.js
@@ -0,0 +1,20 @@
+import http from 'node:http'
+import { Transform } from 'node:stream'
+
+class InverseNumber extends Transform{
+  _transform(chunk, enconding, callback){
+    const transformed = Number(chunk.toString()) * -1
+
+    console.log(transformed)
+
+    callback(null,Buffer.from(String(transformed)))
+  }
+}
+
+const server = http.createServer((req,res) => {
+  return req
+    .pipe(new InverseNumber)
+    .pipe(res)
+})
+
+server.listen(3333)
