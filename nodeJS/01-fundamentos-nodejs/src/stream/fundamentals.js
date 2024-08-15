864e21f78237344f90af9a0449db4bda9beb055a
Tiago Souza <tiago0214@gmail.com>
2024-08-15 13:21:50 +0100

    att

--- nodeJS/01-fundamentos-nodejs/src/stream/fundamentals.js
+++ nodeJS/01-fundamentos-nodejs/src/stream/fundamentals.js
@@ -19,11 +19,27 @@ class OneToAHundredStream extends Readable{
   }
 }
 
+//keep in mind: writable stream never return a value. 
+//it never convert the data in another format it just consume the data. And do something with it.
+//if i need to convert(transform) the data in another format i should use Transform
 class MultiplyByTenStream extends Writable{
   _write(chunk, enconding, callback){
     console.log(Number(chunk.toString()) * 10)
-    callback()
+    callback()//this callback, you'll be the function that'll be called when it did the action "console.log"
   }
 }
 
-new OneToAHundredStream().pipe(new MultiplyByTenStream());
+//transform stream: It is used when i need to transform the data in another thing
+class InverseNumber extends Transform{
+  _transform(chunck, enconding, callback){
+    const transformed = Number(chunck.toString() * -1)
+
+    callback(null, Buffer.from(String(transformed)))// this callback. will be the return value. first param => is the error handle
+  }
+}
+//transform stream => needs to read data from somewhere and send data to somewhere. So it have to be placed in the middle
+//like under.
+
+new OneToAHundredStream()
+  .pipe(new InverseNumber())
+  .pipe(new MultiplyByTenStream());
