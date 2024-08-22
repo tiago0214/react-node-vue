614d7921e2f5c77a446fd16d515083167e321dba
Tiago Souza <tiago0214@gmail.com>
2024-08-22 23:47:28 +0100

    att

--- nodeJS/02-api-rest-nodejs/src/database.ts
+++ nodeJS/02-api-rest-nodejs/src/database.ts
@@ -1,14 +1,11 @@
 import { knex as knexSetup, Knex } from 'knex'
 import 'dotenv/config'
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('Error: trying to connect to database')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
---
+++ nodeJS/02-api-rest-nodejs/src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_URL: z.string(),
+  PORT: z.number().default(3333),
+})
+
+// All the values will be passed to env variable if everthing passes verification.
+const _env = envSchema.safeParse(process.env)
+// parse and safeParse: are the same => difference is that safeParse don't throw an error when the verification failed.
+
+if (_env.success === false) {
+  console.error('🤣 Invalid environmet variable', _env.error.format())
+
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
