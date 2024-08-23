b895cad7904a392f48998d522e79cfc81838a54a
Tiago Souza <tiago0214@gmail.com>
2024-08-23 19:24:56 +0100

    att

---
+++ nodeJS/02-api-rest-nodejs/src/routes/transactions.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify'
+import { knex } from '../database'
+import { z } from 'zod'
+import crypto from 'node:crypto'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const transactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = transactionSchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    })
+
+    return reply.status(201).send()
+  })
+}
