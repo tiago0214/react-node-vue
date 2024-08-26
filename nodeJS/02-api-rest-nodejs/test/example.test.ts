2b30ff1bee2c3b6203e456c2a4f1c8316b9f418b
Tiago Souza <tiago0214@gmail.com>
2024-08-26 19:38:31 +0100

    att

--- nodeJS/02-api-rest-nodejs/test/example.test.ts
+++ nodeJS/02-api-rest-nodejs/test/example.test.ts
@@ -1,21 +1,50 @@
-import { test, expect, beforeAll, afterAll } from 'vitest'
+import { expect, beforeAll, afterAll, it, describe } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 
-beforeAll(async () => {
-  await app.ready()
-})
-
-afterAll(async () => {
-  await app.close()
-})
-
-test('testando', async () => {
-  const response = await request(app.server).post('/transactions').send({
-    title: 'new transaction',
-    amount: 1000,
-    type: 'credit',
+describe('transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+    // ready() => will only be executed when fastify complete to loading my app an its plugins
+    // if i dont this, my app won't be ready in time before the test to be executed.
   })
 
-  expect(response.statusCode).toEqual(201)
+  afterAll(async () => {
+    await app.close()
+    // close() -> will drop my app from memory.
+  })
+
+  it('It should allows users to create a new transaction', async () => {
+    const response = await request(app.server).post('/transactions').send({
+      title: 'new transaction',
+      amount: 1000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('It should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new transaction',
+        amount: 1000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies!)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new transaction',
+        amount: 1000,
+      }),
+    ])
+  })
 })
