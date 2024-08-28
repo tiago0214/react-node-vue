14ad5c96834b1f16d56b969e332062c6aa234fb7
Tiago Souza <tiago0214@gmail.com>
2024-08-28 19:44:47 +0100

    att

--- nodeJS/02-api-rest-nodejs/src/database.ts
+++ nodeJS/02-api-rest-nodejs/src/database.ts
@@ -12,7 +12,7 @@ export const config: Knex.Config = {
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
-    directory: './src/db/migrations',
+    directory: './db/migrations',
   },
 }
 
