361ebfaeca180f3b8bd397cc05115ca456cd96d8
Tiago Souza <tiago0214@gmail.com>
2024-08-30 19:44:51 +0100

    att

--- nodeJS/03-api-solid/src/env/index.ts
+++ nodeJS/03-api-solid/src/env/index.ts
@@ -1,13 +1,14 @@
 import { z } from 'zod'
+import 'dotenv/config'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev','test','production']).default('production'),
-  PORT: z.coerce.number().default(3333)
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('production'),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false){
+if (_env.success === false) {
   console.error('⚠️ invalid environment variables')
 
   throw new Error('Invalid environment variables')
--- nodeJS/03-api-solid/src/server.ts
+++ nodeJS/03-api-solid/src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import {env} from './env/index'
+import { app } from './app'
+import { env } from './env/index'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(()=>{
-  console.log('🚀 server is running!')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 server is running!')
+  })
