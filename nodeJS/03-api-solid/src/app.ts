a6439132b53c4bcb4d260d03ae9cb1e11825ea2d
Tiago Souza <tiago0214@gmail.com>
2024-09-03 17:07:49 +0100

    att

--- nodeJS/03-api-solid/src/app.ts
+++ nodeJS/03-api-solid/src/app.ts
@@ -1,6 +1,25 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from './lib/prisma'
+import { z } from 'zod'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerSchemaBody = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(7),
+  })
+
+  const { name, email, password } = registerSchemaBody.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  reply.status(201).send()
+})
