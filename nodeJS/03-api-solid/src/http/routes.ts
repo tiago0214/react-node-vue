cd04411c6c5adad1d025bdf239f602c0e64994e9
tiago <tiago0214@gmail.com>
2024-09-21 14:25:42 -0300

    att

--- nodeJS/03-api-solid/src/http/routes.ts
+++ nodeJS/03-api-solid/src/http/routes.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { register } from "./controller/register";
 
-export function appRoutes(app: FastifyInstance) {
+export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
 }
