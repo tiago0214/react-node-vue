e8a1ee380e665e31d7b785be7cd5a5c5e81a683c
tiago <tiago0214@gmail.com>
2024-09-25 16:55:11 -0300

    att

--- nodeJS/03-api-solid/src/use-case/register.spec.ts
+++ nodeJS/03-api-solid/src/use-case/register.spec.ts
@@ -1,12 +1,24 @@
 import { expect, describe, it } from "vitest";
 import { RegisterUserCase } from "./register";
-import { PrismaUsersRepository } from "@/repository/prisma/prisma-user-repository";
 import { compare } from "bcryptjs";
 
 describe("Register use case", () => {
   it("should hash user password on registration", async () => {
-    const prismaRepository = new PrismaUsersRepository();
-    const useCase = new RegisterUserCase(prismaRepository);
+    const useCase = new RegisterUserCase({
+      async findByEmail() {
+        return null;
+      },
+
+      async create(data) {
+        return {
+          id: "uuser-1",
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+    });
 
     const { user } = await useCase.execute({
       email: "john@gmail.com",
