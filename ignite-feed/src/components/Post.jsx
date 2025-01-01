ab999f11f724436fdcb02864ab129c5eac22329e
Tiago Souza <tiago0214@gmail.com>
2025-01-01 18:11:43 -0300

    att

--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -1,5 +1,27 @@
+
+import styles from './Post.module.css';
+
 export function Post(){
   return (
-    <h1>Hello word</h1>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/tiago0214.png"/>
+          <div className={styles.authorInfo}>
+            <strong>Tiago Souza</strong>
+            <span>Web developer</span>
+          </div>
+        </div>
+
+        <time title="11 de maio 2020" dateTime="2020-05-11 08:30:00">Publicado há 1H</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Alo galera</p>
+        <p>Tudo na santa paz</p>
+        <p>Hi everyone</p>
+        <p><a href="#">Finishing</a></p>
+      </div>
+    </article>
   )
 }
