36df6305b4510ec7f92a1590304a171f45707744
Tiago Souza <tiago0214@gmail.com>
2025-01-09 23:03:44 -0300

    att

--- react/ignite-feed/src/App.jsx
+++ react/ignite-feed/src/App.jsx
@@ -4,16 +4,83 @@ import styles from './App.module.css';
 import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 
+const post = [
+  {
+    id: 1,
+    author: {
+      name: 'Tiago Souza',
+      avatarUrl: 'https://github.com/tiago0214.png',
+      role: 'Web developer'
+    },
+    publishedAt: new Date('2024-05-11 08:30:00'),
+    content: [
+      {
+      type: 'paragraph',
+      content: 'Alo galera'
+      }, 
+      {
+      type: 'paragraph',
+      content: 'Tudo na santa paz'
+      },
+      {
+      type: 'paragraph',
+      content: 'Hi everyone'
+      }, 
+      {
+      type: 'link',
+      content: 'Finishing'
+      }
+    ]
+  },
+  {
+    id: 2,
+    author: {
+      name: 'Matheus Gome',
+      avatarUrl: 'https://github.com/matheus0214.png',
+      role: 'Web developer'
+    },
+    publishedAt: new Date('2024-09-11 08:30:00'),
+    content: [
+      {
+      type: 'paragraph',
+      content: 'Alo galera'
+      }, 
+      {
+      type: 'paragraph',
+      content: 'Tudo na santa paz'
+      },
+      {
+      type: 'paragraph',
+      content: 'Hi everyone'
+      }, 
+      {
+      type: 'link',
+      content: 'Finishing'
+      }
+    ]
+  }
+]
+
+
+
 export function App() {
-  return (
+  return ( 
     <div>
       <Header/>
       <div className={styles.wrapper}>
         <Sidebar/>
         
         <main>
-          <Post/>
-          <Post/>
+          {post.map(post =>{
+            return(
+              <Post
+                key={post.id}
+                author={post.author}
+                publishedAt={post.publishedAt}
+                content={post.content}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
