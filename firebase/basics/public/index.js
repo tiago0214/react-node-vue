db027f539c091e61cc9171f846d4a4f457ec4f58
Tiago Souza <tiago0214@gmail.com>
2025-01-15 18:17:57 -0300

    att

--- firebase/basics/public/index.js
+++ firebase/basics/public/index.js
@@ -1,26 +1,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { getFirestore, collection, addDoc  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
+import { getFirestore, collection, addDoc, query, where, onSnapshot  } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 
 import { faker } from 'https://esm.sh/@faker-js/faker';
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyDguS7x6pM9o-lYwBFUl1BCzsWgvEbhBL4",
-
   authDomain: "my-app-1aeef.firebaseapp.com",
-
   projectId: "my-app-1aeef",
-
   storageBucket: "my-app-1aeef.firebasestorage.app",
-
   messagingSenderId: "1014789487676",
-
   appId: "1:1014789487676:web:26230a7bd366a935805739"
-
 };
-const app = initializeApp(firebaseConfig);
 
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
@@ -53,11 +46,11 @@ const createThing = document.getElementById('createThing');
 const thingsList = document.getElementById('thingsList');
 
 let thingsRef = collection(db,"things")
+let q;
 let unsubscribe;
 
 onAuthStateChanged(auth, (user) => {
   if(user){
-
     createThing.onclick = async () => {
       await addDoc(thingsRef, {
         uid: user.uid,
@@ -65,5 +58,25 @@ onAuthStateChanged(auth, (user) => {
         weight: faker.number.int()
       });
     }
+
+    createThing.hidden = false;
+
+    q = query(thingsRef, where('uid','==', user.uid))
+
+    unsubscribe = onSnapshot(q, (querySnapshot) =>{
+      const items = [];
+
+      querySnapshot.forEach((doc) => {
+        items.push(`<li>${doc.data().name}</li>`)
+      })
+
+      thingsList.innerHTML = items.join('');
+    })
+      
+  }
+  else{
+    createThing.hidden = true;
+
+    unsubscribe && unsubscribe();
   }
 })
