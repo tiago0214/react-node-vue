cd6b5e328d9089d377d5716a6974b4ddfcfb20ba
Tiago Souza <tiago0214@gmail.com>
2025-01-17 23:16:57 -0300

    att

--- react/timer/eslint.config.js
+++ react/timer/eslint.config.js
@@ -1,28 +1,33 @@
-import js from '@eslint/js'
 import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import prettier from 'eslint-plugin-prettier'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  { plugins: { prettier } },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
       ],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
     },
   },
-)
+]
--- react/timer/src/components/Button.styles.ts
+++ react/timer/src/components/Button.styles.ts
@@ -2,22 +2,9 @@ import styled from 'styled-components';
 
 export type Variant = 'primary' | 'secondary' | 'success' | 'danger';
 
-interface ButtonContainerProps{
-  variant: Variant
-}
-
-const getBackgroundColor = {
-  primary: '#007bff',
-  secondary: '#6c757d',
-  success: '#28a745',
-  danger: '#dc3545'
-}
-
-export const ButtonContainer = styled.button<ButtonContainerProps>`
+export const ButtonContainer = styled.button`
   width: 100px;
   height: 40px;
 
-  ${props => {
-    return `background-color: ${getBackgroundColor[props.variant]};`
-  }}
+  background-color: ${props => props.theme['green-500']};
 `
