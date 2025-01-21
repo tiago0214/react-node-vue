aed65e48c4818f30a7054561e849428f98ca6b47
Tiago Souza <tiago0214@gmail.com>
2025-01-21 18:30:11 -0300

    att

--- react/02-timer/src/@types/styled.d.ts
+++
@@ -1,9 +0,0 @@
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
-
-//eu estou pegando a inferência de tipo do typescript, e colocando fixo no ThemeType
-type ThemeType = typeof defaultTheme;
-
-declare module 'styled-components'{
-  export interface DefaultTheme extends ThemeType{}
-}
