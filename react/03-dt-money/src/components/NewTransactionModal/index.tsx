d111b0ebb02abc069054a8311a26e101469b2f2c
Tiago Souza <tiago0214@gmail.com>
2025-02-08 22:43:10 -0300

    att

--- react/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ react/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { CloseButton, Content, Overlay } from './styles'
-import { X } from '@phosphor-icons/react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles'
+import { ArrowCircleDown, ArrowCircleUp, X } from '@phosphor-icons/react'
 
 export function NewTransactionModal(){
   return (
@@ -18,6 +18,19 @@ export function NewTransactionModal(){
           <input type="text" placeholder='Descrição' required />
           <input type="number" placeholder='Preço'required />
           <input type="text" placeholder='Categoria'required />
+
+          <TransactionType>
+            <TransactionTypeButton value='income' variant='income'>
+              <ArrowCircleUp size={24}/>
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton value='outcome' variant='outcome'>
+              <ArrowCircleDown size={24}/>
+              Saida
+            </TransactionTypeButton>
+
+          </TransactionType>
 
           <button type='submit'>Cadastrar</button>
         </form>
