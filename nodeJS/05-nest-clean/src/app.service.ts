122497808b1c276b78c2db30fa72a90e47ceba55
Tiago Souza <tiago0214@gmail.com>
2025-03-25 16:39:31 -0300

    att

--- nodeJS/05-nest-clean/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common'
-
-@Injectable()
-export class AppService {
-  hello(): string {
-    return 'Hello World!'
-  }
-}
---
+++ nodeJS/05-nest-clean/src/controllers/create-account.controller.ts
@@ -0,0 +1,42 @@
+import { Body, ConflictException, Controller, Post } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { hash } from 'bcryptjs'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(4),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
+
+@Controller('/accounts')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = createAccountBodySchema.parse(body)
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userWithSameEmail) {
+      throw new ConflictException('Email already in use')
+    }
+
+    const hashedPassword = await hash(password, 8)
+
+    await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    })
+  }
+}
