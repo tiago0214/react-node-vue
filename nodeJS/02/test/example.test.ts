38904605b73b3f44ebd478efe96b0b5ca531e5ce
Tiago Souza <tiago0214@gmail.com>
2025-07-24 23:55:21 -0300

    att

--- nodeJS/02/test/example.test.ts
+++ nodeJS/02/test/example.test.ts
@@ -1,7 +1,18 @@
-import { test, expect } from 'vitest'
+import { afterAll, beforeAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('Somente test', () => {
-  const statusCode = 201
+beforeAll(async () => await app.ready())
 
-  expect(statusCode).toBe(201)
+afterAll(async () => await app.close())
+
+test('User should be able to create a new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 1200,
+      type: 'credit',
+    })
+    .expect(201)
 })
