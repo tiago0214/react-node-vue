604ba13147c81caba7e16b6e9184cfb356954769
Tiago Souza <tiago0214@gmail.com>
2025-07-30 01:55:13 -0300

    att

---
+++ nodeJS/02/test/transactions.test.ts
@@ -0,0 +1,50 @@
+import { afterAll, beforeAll, it, describe, expect, beforeEach } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+import { execSync } from 'child_process'
+
+describe('Transactions routes', () => {
+  beforeAll(async () => await app.ready())
+
+  afterAll(async () => await app.close())
+
+  beforeEach(() => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
+  })
+
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 1200,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New Transaction',
+        amount: 1200,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies!)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New Transaction',
+        amount: 1200,
+      }),
+    ])
+  })
+})
